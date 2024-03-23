c07d8d44403b22b27beab6fef2d985069f1b6caa
MaxDYF <1048897180@qq.com>
2024-03-23 11:44:04 +0800

    update quickPow module

--- String Processing/String Hash/string hash.cpp
+++ String Processing/String Hash/string hash.cpp
@@ -1,4 +1,4 @@
-//#pragma GCC optimize(2)
+// #pragma GCC optimize(2)
 #include <bits/stdc++.h>
 
 const int N = 2e5 + 10;
@@ -6,7 +6,7 @@ const int inf = 1 << 30;
 const long long llinf = 1ll << 60;
 const double PI = acos(-1);
 
-#define lowbit(x) (x&-x)
+#define lowbit(x) (x & -x)
 /*
  *	字符串哈希模板
  *	使用namespace进行封装
@@ -19,7 +19,7 @@ namespace stringHash
 	const int64_t CHAR_SIZE = 26; // 字符集大小
 	HashPair hashPow[N];
 	const int64_t mod1 = 100000007LL,
-			 	  mod2 = 100000037LL;
+				  mod2 = 100000037LL;
 
 	/*
 	 *	预处理函数
@@ -31,7 +31,7 @@ namespace stringHash
 		hashPow[0] = std::make_pair(1LL, 1LL);
 		for (int i = 1; i < N; i++)
 		{
-			hashPow[i].first  = hashPow[i - 1].first  * (CHAR_SIZE + 1LL) % mod1;
+			hashPow[i].first = hashPow[i - 1].first * (CHAR_SIZE + 1LL) % mod1;
 			hashPow[i].second = hashPow[i - 1].second * (CHAR_SIZE + 1LL) % mod2;
 		}
 	}
@@ -43,22 +43,22 @@ namespace stringHash
 	{
 		return (int64_t)(ch - 'A' + 1);
 	}
-	
+
 	/*
 	 *	序列生成Hash生成函数
 	 *  返回一个std::vector<HashPair>
 	 *	储存生成出的Hash序列
-	 */ 
+	 */
 	std::vector<HashPair> makeHash(std::string s)
 	{
 		size_t len = s.length();
 		std::vector<HashPair> hashVal(len);
 		for (size_t i = 0; i < len; i++)
 		{
-			hashVal[i].first  = (i > 0) ? (hashVal[i - 1].first  * (CHAR_SIZE + 1LL) + transChar(s[i])) % mod1
-								   : transChar(s[i]) % mod1;
+			hashVal[i].first = (i > 0) ? (hashVal[i - 1].first * (CHAR_SIZE + 1LL) + transChar(s[i])) % mod1
+									   : transChar(s[i]) % mod1;
 			hashVal[i].second = (i > 0) ? (hashVal[i - 1].second * (CHAR_SIZE + 1LL) + transChar(s[i])) % mod2
-								   : transChar(s[i]) % mod1;						
+										: transChar(s[i]) % mod1;
 		}
 		return hashVal;
 	}
@@ -75,10 +75,12 @@ namespace stringHash
 		else
 		{
 			HashPair result;
-			result.first  = (hashVal[r - 1].first - 
-							 hashVal[l - 1].first  * hashPow[r - l].first  % mod1 + mod1) % mod1;
-			result.second = (hashVal[r - 1].second - 
-							 hashVal[l - 1].second * hashPow[r - l].second % mod2 + mod2) % mod2;
+			result.first = (hashVal[r - 1].first -
+							hashVal[l - 1].first * hashPow[r - l].first % mod1 + mod1) %
+						   mod1;
+			result.second = (hashVal[r - 1].second -
+							 hashVal[l - 1].second * hashPow[r - l].second % mod2 + mod2) %
+							mod2;
 			return result;
 		}
 	}
@@ -97,7 +99,6 @@ void work()
 		if (stringHash::subHash(a, i - len, i) == hash)
 			ans++;
 	std::cout << ans << '\n';
-		
 }
 int main()
 {
@@ -107,6 +108,6 @@ int main()
 	stringHash::preProcess();
 	int t;
 	std::cin >> t;
-	while (t --> 0)
+	while (t-- > 0)
 		work();
 }
