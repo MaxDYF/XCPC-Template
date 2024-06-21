a74617c9f168830f5a9ca0ed7b8ceabcbbd863c8
maxdyf <1048897180@qq.com>
2024-06-21 21:06:17 +0800

    新增计算几何模块

--- 数学/计算几何.cpp
+++ 数学/计算几何.cpp
@@ -2,105 +2,171 @@
 using namespace std;
 typedef double design_float;
 const design_float PI = acos(-1);
-const design_float EPS = 1e-9;
-class Vector
+const design_float eps = 1e-7;
+struct Vector
 {
-    friend class Point;
-    friend class Line;
-    friend vector<Point> getHull(vector<Point> &points);
-
-private:
     design_float x, y;
-
-public:
-    Vector(design_float x, design_float y) : x(x), y(y) {}
-    Vector(design_float angle) : x(cos(angle)), y(sin(angle)) {}
-    Vector(Point a, Point b) : x(b.x - a.x), y(b.y - a.y) {}
-    Vector(Point a) : x(a.x), y(a.y) {}
-    Vector operator+(Vector b)
+    Vector(design_float x = 0, design_float y = 0)
+        : x(x), y(y)
     {
-        return Vector{x + b.x, y + b.y};
     }
-    Vector operator-(Vector b)
+    Vector operator+(const Vector &b) const { return Vector(x + b.x, y + b.y); }
+    Vector operator-(const Vector &b) const { return Vector(x - b.x, y - b.y); }
+    Vector operator*(const design_float &b) const { return Vector(x * b, y * b); }
+    Vector operator/(const design_float &b) const { return Vector(x / b, y / b); }
+    design_float operator*(const Vector &b) const { return x * b.x + y * b.y; }
+    // 重定义^为叉乘
+    design_float operator^(const Vector &b) const { return x * b.y - y * b.x; }
+    design_float length() const { return sqrt(x * x + y * y); }
+    design_float angle() const { return atan2(y, x); }
+    Vector unit() const { return *this / length(); }
+    // 将向量旋转angle角度，angle为弧度制
+    Vector rotate(design_float angle) const
     {
-        return Vector{x - b.x, y - b.y};
-    }
-    Vector operator*(design_float k)
-    {
-        return Vector{x * k, y * k};
-    }
-    Vector operator/(design_float k)
-    {
-        return Vector{x / k, y / k};
-    }
-    Vector operator*(Vector b)
-    {
-        return Vector{x * b.x, y * b.y};
-    }
-    design_float operator^(const Vector &b)
-    {
-        return x * b.y - y * b.x;
+        return Vector(x * cos(angle) - y * sin(angle), x * sin(angle) + y * cos(angle));
     }
 };
-class Point
+typedef Vector Point;
+struct Line
 {
-    friend class Vector;
-    friend class Line;
-    friend vector<Point> getHull(vector<Point> &points);
-
-private:
-    design_float x, y;
-
-public:
-    Point(design_float x, design_float y) : x(x), y(y) {}
-    Point operator+(Vector b)
+    Point a, b;
+    Vector vec() { return b - a; }
+    Line(Point a = Point(), Point b = Point())
+        : a(a), b(b)
     {
-        return Point{x + b.x, y + b.y};
     }
-    Vector operator-(Point b)
+    // 判断点c在直线上
+    bool isPointOnLine(Point c) { return ((b - a) ^ (c - a)) == 0; }
+    // 判断点c在线段上
+    bool isPointOnSegment(Point c)
     {
-        return Vector{x - b.x, y - b.y};
+        return ((b - a) ^ (c - a)) == 0 && (c - a) * (c - b) <= 0;
     }
-    Point operator*(design_float k)
+    // 求点c到直线的距离
+    design_float distanceToLine(Point c)
     {
-        return Point{x * k, y * k};
+        return fabs(((b - a) ^ (c - a)) / (b - a).length());
     }
-    Point operator/(design_float k)
+    // 求点c到线段的距离
+    design_float distanceToSegment(Point c)
     {
-        return Point{x / k, y / k};
+        if ((c - a) * (b - a) < 0)
+            return (c - a).length();
+        if ((c - b) * (a - b) < 0)
+            return (c - b).length();
+        return distanceToLine(c);
     }
-    design_float operator^(Point b)
+    // 求点c到线段的最近点
+    Point nearestPointToSegment(Point c)
     {
-        return x * b.y - y * b.x;
-    }
-    void rotate(design_float angle)
-    {
-        design_float x1 = x * cos(angle) - y * sin(angle);
-        design_float y1 = x * sin(angle) + y * cos(angle);
-        x = x1;
-        y = y1;
+        if ((c - a) * (b - a) < 0)
+            return a;
+        if ((c - b) * (a - b) < 0)
+            return b;
+        design_float r = (c - a) * (b - a) / (b - a).length();
+        return a + (b - a).unit() * r;
     }
 };
-class Line
+namespace Geometry
 {
-private:
-    design_float st_x, st_y, ed_x, ed_y;
-
-public:
-    Line(design_float st_x, design_float st_y, design_float ed_x, design_float ed_y) : st_x(st_x), st_y(st_y), ed_x(ed_x), ed_y(ed_y) {}
-    Line(Point a, Point b) : st_x(a.x), st_y(a.y), ed_x(b.x), ed_y(b.y) {}
-    Line(Point a, Vector b) : st_x(a.x), st_y(a.y), ed_x(a.x + b.x), ed_y(a.y + b.y) {}
-    design_float length()
+    Point intersection(Line l1, Line l2)
     {
-        return sqrt((st_x - ed_x) * (st_x - ed_x) + (st_y - ed_y) * (st_y - ed_y));
+        design_float s1 = (l2.b - l2.a) ^ (l1.a - l2.a);
+        design_float s2 = (l2.b - l2.a) ^ (l1.b - l2.a);
+        return (l1.a * s2 - l1.b * s1) / (s2 - s1);
     }
-    design_float angle()
+    // 输入一个点集，返回凸包
+    vector<Point> getHull(vector<Point> p)
     {
-        return atan2(ed_y - st_y, ed_x - st_x);
+        int n = p.size();
+        sort(p.begin(), p.end(), [](Point a, Point b)
+             { return a.x == b.x ? a.y < b.y : a.x < b.x; });
+        vector<Point> res;
+        for (int i = 0; i < n; i++)
+        {
+            while (res.size() > 1 && ((res[res.size() - 1] - res[res.size() - 2]) ^ (p[i] - res[res.size() - 1])) <= 0)
+                res.pop_back();
+            res.push_back(p[i]);
+        }
+        int t = res.size();
+        for (int i = n - 2; i >= 0; i--)
+        {
+            while (res.size() > t && ((res[res.size() - 1] - res[res.size() - 2]) ^ (p[i] - res[res.size() - 1])) <= 0)
+                res.pop_back();
+            res.push_back(p[i]);
+        }
+        res.pop_back();
+        return res;
     }
-};
-
-
+    // 计算凸包的边长
+    design_float getHullLength(vector<Point> hull)
+    {
+        design_float res = 0;
+        for (int i = 0; i < hull.size(); i++)
+            res += (hull[i] - hull[(i + 1) % hull.size()]).length();
+        return res;
+    }
+    // 计算凸包的面积
+    design_float getHullArea(vector<Point> hull)
+    {
+        design_float res = 0;
+        for (int i = 0; i < hull.size(); i++)
+            res += (hull[i] ^ hull[(i + 1) % hull.size()]);
+        return fabs(res) / 2;
+    }
+    bool pointOnLeft(Point p, Line l)
+    {
+        return ((l.b - l.a) ^ (p - l.a)) > eps;
+    }
+    // 求半平面交，给定若干条有向线段，算法将其排序后，以左边为半平面，返回交的凸包。
+    vector<Point> getHalfPlane(vector<Line> lines)
+    {
+        sort(lines.begin(), lines.end(), [](auto a, auto b)
+             {
+            design_float angle1 = a.vec().angle(), angle2 = b.vec().angle();
+            if (fabs(angle1 - angle2) < eps)
+                return pointOnLeft(a.a, b);
+            else
+                return angle1 < angle2; });
+        vector<Line> res;
+        for (auto x : lines)
+        {
+            if (!res.empty() && fabs(x.vec().angle() - res.back().vec().angle()) < eps)
+                continue;
+            res.push_back(x);
+        }
+        lines.swap(res);
+        int len = lines.size();
+        int l = 1, r = 0;
+        vector<int> q(len * 2);
+        vector<Point> p(len * 2);
+        for (int i = 0; i < len; i++)
+        {
+            while (l < r && !pointOnLeft(p[r], lines[i]))
+                r--;
+            while (l < r && !pointOnLeft(p[l + 1], lines[i]))
+                l++;
+            q[++r] = i;
+            if (l < r && fabs((lines[q[r]].vec() ^ lines[q[r - 1]].vec())) < eps)
+                if (lines[q[r]].vec() * lines[q[r - 1]].vec() < -eps)
+                    return vector<Point>();
+            if (l < r)
+                p[r] = intersection(lines[q[r]], lines[q[r - 1]]);
+        }
+        while (l < r && !pointOnLeft(p[r], lines[q[l]]))
+            r--;
+        if (r - l <= 1)
+            return vector<Point>();
+        p[l] = intersection(lines[q[r]], lines[q[l]]);
+        vector<Point> ans;
+        while (l <= r)
+        {
+            ans.push_back(p[l]);
+            l++;
+        }
+        return ans;
+    }
+}
 int main()
 {
 }
